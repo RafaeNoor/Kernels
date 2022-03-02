6a865249dad429a409ea124aef109e27812ef7d2
Aaron Councilman <counc009@umn.edu>
2022-03-02 09:33:59 -0600

    Additional kernels and code and assignment write-up

--- Kernels.cc
+++ Kernels.cc
@@ -1,157 +1,329 @@
-#include <iostream>
+#include <cassert>
 #include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
-#define TILE_I 4
-#define TILE_J 4
-#define TILE_K 2
+// Definition of a matrix of integers of a given size
+class Matrix {
+private:
+  unsigned rows_;
+  unsigned cols_;
+  shared_ptr<int[]> data;
 
-struct Tensor_t {
-    float* data;
-    unsigned rows;
-    unsigned cols;
+  // Class used to enable accessing elements from a Matrix using a M[r][c]
+  // form, only defines the [] operator. Is constructed from the beginning of
+  // the row in the matrix, and the number of columns (for dynamic bounds
+  // checking)
+  class Row {
+    private:
+      int* array;
+      const unsigned cols;
+    public:
+      Row(int* data, unsigned cols_) : array(data), cols(cols_) {}
+      int& operator[](int col) const {
+        assert(0 <= col && col < cols && "Matrix access out of range");
+        return array[col];
+      }
+  };
+public:
+  Matrix(unsigned rs, unsigned cs) : rows_(rs), cols_(cs),
+                                     data(new int[rs * cs])
+  {
+    for (int i = 0; i < rows_ * cols_; i++) data[i] = 0;
+  }
 
-    Tensor_t(unsigned rows_, unsigned cols_ ) 
-        : rows(rows_), cols(cols_){
-            data = new float[rows_ * cols_];
-            for(int k = 0; k < rows_ * cols_; k++) data[k] = 0.0;
-        }
+  // Accessors
+  unsigned rows() const { return rows_; }
+  unsigned cols() const { return cols_; }
 
-    void randomize(){
-        for(int k = 0; k < rows * cols; k++) 
-            data[k]  = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5.0));
+  // Fills a matrix by calling a given generator function at each location in
+  // the matrix
+  void fill(std::function<int(int,int)> generator) {
+    for (int r = 0; r < rows_; r++) {
+      for (int c = 0; c < cols_; c++) {
+        (*this)[r][c] = generator(r, c);
+      }
+    }
+  }
+
+  friend ostream& operator<<(ostream& os, const Matrix& m) {
+    os << "Rows " << m.rows_ << ", Cols " << m.cols_ << "\n";
+    for(int r = 0; r < m.rows_; r++){
+      os << "[ ";
+      for(int c = 0; c < m.cols_; c++){
+        os << m[r][c] <<" ";
+      }
+      os << "] " << "\n";
+    }
+    return os;
+  }
+
+  // Operator overloading to access a row of the matrix
+  Row operator[](int row) const {
+    assert(0 <= row && row < rows_ && "Matrix access out of range");
+    return Row(&(data[row * cols_]), cols_);
+  }
+
+  bool operator==(Matrix& other) {
+    if (rows_ != other.rows_ || cols_ != other.cols_) return false;
+
+    for (int r = 0; r < rows_; r++) {
+      for (int c = 0; c < cols_; c++) {
+        if ((*this)[r][c] != other[r][c]) return false;
+      }
     }
 
-    void print(){
-        cout << "Rows:\t"<< rows <<"\n";
-        cout << "Cols:\t"<< cols <<"\n";
-
-        for(int i = 0; i < rows; i++){
-            cout << "[ ";
-            for(int j = 0; j < cols; j++){
-                cout << data[i*cols + j] <<" ";
-
-            }
-            cout << "] " << "\n";
-        }
-
-    }
-
-
+    return true;
+  }
 };
 
-int min(int a , int b){
-  return (a < b)? a : b;
-
+int min(int a, int b){
+  return (a < b) ? a : b;
 }
 
-Tensor_t Convolution(Tensor_t Input, Tensor_t Kernel){
-    unsigned Output_Rows = Input.rows - Kernel.rows + 1;
-    unsigned Output_Cols = Input.cols - Kernel.cols + 1;
+// ============================================================================
+// Kernel 1: Matrix transpose.
+// ============================================================================
+Matrix Transpose(Matrix& A) {
+  const unsigned aRows = A.rows();
+  const unsigned aCols = A.cols();
 
-    Tensor_t Output(Output_Rows, Output_Cols);
+  Matrix output(aCols, aRows);
 
-    for(int i = 0; i < Output_Rows; i++){
-        for(int j = 0; j < Output_Cols; j++){
-            for(int ki = i; ki < i + Kernel.rows; ki++){
-                for(int kj = j; kj < j + Kernel.cols; kj++){
-                    float Input_Value = Input.data[ki* Input.cols + kj]; 
-                    float Kernel_Value = Kernel.data[(ki-i)*Kernel.cols + (kj-j)];
-                    Output.data[i*Output_Cols + j] += Input_Value * Kernel_Value;
-
-                }
-            }
-        }
+  for (int i = 0; i < aRows; i++) {
+    for (int j = 0; j < aCols; j++) {
+      output[j][i] = A[i][j];
     }
+  }
 
-
-    return Output;
-
+  return output;
 }
 
-Tensor_t MatrixMultiply(Tensor_t A, Tensor_t B){
+// ============================================================================
+// Kernel 2: Matrix convolution.
+// ============================================================================
+Matrix Convolution(Matrix& input, Matrix& kernel) {
+  const unsigned inRows = input.rows();
+  const unsigned inCols = input.cols();
 
-    Tensor_t Output(A.rows, B.cols);
+  const unsigned kRows = kernel.rows();
+  const unsigned kCols = kernel.cols();
 
-    for(int i = 0; i < A.rows; i+= TILE_I){
-        int imax = min(i+TILE_I,A.rows);
-        for(int j =0; j < B.cols; j+= TILE_J){
-            int jmax = min(j+TILE_J,B.cols);
-            for(int k = 0; k < A.cols; k+=TILE_K){
-                int kmax = min(k+TILE_K, A.cols);
-                for(int ii = i; ii < imax ; ii++){
-                    for(int jj =j; jj < jmax; jj++){
-                        for(int kk = k; kk < kmax; kk++){
-                            Output.data[ii*B.cols + jj] += A.data[ii*A.cols + kk] * B.data[kk * B.cols + jj];
-                        }
-                    }
-                }
-            }
+  const unsigned outRows = inRows - kRows + 1;
+  const unsigned outCols = inCols - kCols + 1;
+
+  Matrix output(outRows, outCols);
+
+  for (int i = 0; i < outRows; i++) {
+    for (int j = 0; j < outCols; j++) {
+      for (int ki = i; ki < i + kRows; ki++) {
+        for (int kj = j; kj < j + kCols; kj++) {
+          int inVal = input[ki][kj];
+          int kVal  = kernel[ki-i][kj-j];
+          output[i][j] += inVal * kVal;
         }
+      }
     }
+  }
 
-    return Output;
-
-
+  return output;
 }
 
-Tensor_t Transpose(Tensor_t A){
-    Tensor_t Output(A.cols, A.rows);
+// ============================================================================
+// Kernel 3: Matrix multiplication.
+// ============================================================================
+Matrix MatrixMultiply(Matrix& A, Matrix& B) {
+  const unsigned aRows = A.rows();
+  const unsigned aCols = A.cols();
 
-    for(int i = 0; i < A.rows; i++){
-        for(int j = 0; j < A.cols; j++){
-            Output.data[j* A.rows + i] = A.data[i*A.cols + j];
+  const unsigned bRows = B.rows();
+  const unsigned bCols = B.cols();
+
+  assert(aCols == bRows && "Incompatible matrix multiplication dimensions");
+
+  Matrix output(aRows, bCols);
+
+  for (int i = 0; i < aRows; i++) {
+    for (int j = 0; j < aCols; j++) {
+      for (int k = 0; k < bCols; k++) {
+        output[i][k] += A[i][j] * B[j][k];
+      }
+    }
+  }
+
+  return output;
+}
+
+// Helper function for Kernel 4: Computes the multiplicative inverse of a
+// modulo m
+int inverse(int a, int m);
+
+// ============================================================================
+// Kernel 4: Matrix inverse (modulo m)
+// ============================================================================
+Matrix Invert(Matrix& A, int m) {
+  const unsigned aRows = A.rows();
+  const unsigned aCols = A.cols();
+
+  assert(aRows == aCols && "Cannot invert non-square matrix");
+  assert(m > 0 && "Invalid modulo");
+
+  const unsigned n = aRows;
+
+  Matrix Aug(n, n * 2);
+  Aug.fill([&A, n](int r, int c) {
+              if (c < n) return A[r][c];
+              else return (c-n) == r ? 1 : 0; });
+
+  for (int r = 0; r < n; r++) {
+    int factor = inverse(Aug[r][r], m);
+    if (factor == 0) throw "Non-invertible matrix";
+
+    for (int c = r; c < 2 * n; c++) Aug[r][c] = (Aug[r][c] * factor) % m;
+    for (int r2 = 0; r2 < n; r2++) {
+      if (r2 != r) {
+        if (Aug[r2][r] != 0) {
+          int factor = Aug[r2][r];
+          for (int c = r; c < 2 * n; c++)
+            Aug[r2][c] = (Aug[r2][c] - factor * Aug[r][c]) % m;
         }
+      }
     }
+  }
 
-    return Output;
+  Matrix Inverse(n, n);
+  Inverse.fill([&Aug, n](int r, int c) { return Aug[r][n + c]; });
+  return Inverse;
 }
 
+// ============================================================================
+// Kernel 5: Product of a list of matrices
+//  Assumption: for parallelizing this kernel you may assume that all matrices
+//    are square n x n matrices. For Challenge 1 we ask you to define a
+//    parallel version without this assumption
+// ============================================================================
+Matrix matrixProduct(vector<Matrix> matrices) {
+  if (matrices.size() == 0) throw "Product of 0 matrices is undefined";
 
-int main(int argc, char** argv){
+  // Reminder: matrix multiplication is associative and the identity matrix
+  // is the multiplicative identity (i.e. for any matrix A, I A = A)
+  Matrix prod = matrices[0];
+  for (int i = 1; i < matrices.size(); i++) {
+    prod = MatrixMultiply(prod, matrices[i]);
+  }
+
+  return prod;
+}
+
+// ============================================================================
+// Main driver, contains examples using the above kernels
+// Feel free to modify this to more extensively test your parallelized kernels
+// ============================================================================
+int main(int argc, char** argv) {
+  Matrix input(4, 4);
+  Matrix kernel(3, 3);
+
+  input.fill([](int, int) {
+              static unsigned int seed = 0;
+              return rand_r(&seed) / (RAND_MAX / 5.0f);
+            });
+  kernel.fill([](int, int) {
+              static unsigned int seed = 314159;
+              return rand_r(&seed) / (RAND_MAX / 5.0f);
+            });
+
+  auto output = Convolution(input, kernel);
+
+  cout << "====================\n";
+  cout << "Input: "  << input;
+  cout << "Kernel: " << kernel;
+  cout << "Output: " << output;
+
+  Matrix A(4, 2);
+  Matrix B(2, 6);
+
+  A.fill([](int r, int c) { return r + c; });
+  B.fill([](int, int) { static int i = 0; return i++; });
+
+  auto prod  = MatrixMultiply(A, B);
+
+  cout << "====================\n";
+  cout << "A: "    << A;
+  cout << "B: "    << B;
+  cout << "A x B:" << prod;
+
+  auto AT = Transpose(A);
+  auto BT = Transpose(B);
+  auto prodT = Transpose(prod);
+  auto prodOT = MatrixMultiply(BT, AT);
+
+  cout << "====================\n";
+  cout << "A^T: " << AT;
+  cout << "B^T: " << BT;
+  cout << "(A x B)^T: " << prodT;
+  cout << "B^T x A^T: " << prodOT;
+
+  cout << "A == B? " << (A == B ? "true" : "false") << "\n";
+  cout << "A == A^T? " << (A == AT ? "true" : "false") << "\n";
+  cout << "(A x B)^T == B^T x A^T? " << (prodT == prodOT ? "true" : "false") << "\n";
+
+  Matrix M(2, 2);
+  M.fill([](int r, int c) { return c + 2 * r + 1; });
+  auto Inv = Invert(M, 7);
+  auto Id  = MatrixMultiply(M, Inv); // This is an identity matrix (mod 7)
+
+  cout << "====================\n";
+  cout << "M: "      << M;
+  cout << "M^-1: "   << Inv;
+  cout << "M M^-1: " << Id;
+
+  Matrix M1(3, 3); M1.fill([](int r, int c) { return r + c; });
+  Matrix M2(3, 3); M2.fill([](int r, int c) { return r * c; });
+  Matrix M3(3, 3); M3.fill([](int r, int c) { return r % (c + 1); });
+  Matrix M4(3, 3); M4.fill([](int r, int c) { return c % (r + 1); });
+
+  std::vector<Matrix> matrices = {M1, M2, M3, M4};
+
+  Matrix res = matrixProduct(matrices);
+  cout << "====================\n";
+  cout << "Product: " << res;
+  cout << "====================\n";
 
 
-    Tensor_t Input(4,4);
-    Tensor_t Kernel(3,3);
+  return 0;
+}
 
-    Input.randomize();
-    Kernel.randomize();
+// ============================================================================
+// Helper function for Kernel 4
+// Computes the multiplicative inverse of a modulo m
+// Uses the Extended Euclidean Algorithm
+// ============================================================================
+int inverse(int a, int m) {
+  assert(m > 0 && "Invalid modulo");
 
-    Tensor_t Output = Convolution(Input, Kernel);
+  a %= m;
+  if (a < 0) a += m;
 
-    cout << "Input: "<<"\n";
-    Input.print();
+  int b = m;
 
+  int old_r = a, r = b;
+  int old_s = 1, s = 0;
 
+  while (r != 0) {
+    int q = old_r / r;
+    int tmp;
 
-    cout << "Kernel: "<<"\n";
-    Kernel.print();
+    tmp = old_r - q * r;
+    old_r = r; r = tmp;
 
+    tmp = old_s - q * s;
+    old_s = s; s = tmp;
+  }
 
-
-    cout << "Output: "<<"\n";
-    Output.print();
-
-
-    Tensor_t A(4,2);
-    Tensor_t B(2,6);
-
-    A.randomize();
-    B.randomize();
-
-    Tensor_t AxB = MatrixMultiply(A,B);
-
-    A.print();
-    B.print();
-
-    AxB.print();
-
-    Tensor_t AxBt = Transpose(AxB);
-    AxBt.print();
-
-
-
-
-    return 0;
+  return old_r == 1 ? old_s : 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# Kernels
+# Program Parallelization
+In this assignment, we ask you to parallelize some matrix kernels that we've
+defined in [Kernels.cc](./Kernels.cc). There are five kernels:
+
+1. A [matrix transpose](https://en.wikipedia.org/wiki/Transpose)
+2. A [convolution](https://en.wikipedia.org/wiki/Kernel_(image_processing))
+3. A [matrix multiplication](https://en.wikipedia.org/wiki/Matrix_multiplication)
+4. A [matrix inverse](https://en.wikipedia.org/wiki/Invertible_matrix) [modulo m](https://en.wikipedia.org/wiki/Modular_arithmetic)
+5. A product of a list of matrices
+
+For this assignment, we are asking you to parallelize each of these benchmarks
+(if possible) using OpenMP.
+A relatively simple introduction to OpenMP can be found [here](https://medium.com/swlh/introduction-to-the-openmp-with-c-and-some-integrals-approximation-a7f03e9ebb65)
+and there are many other tutorials and sources of documentation available
+online.
+For these kernels you can primarily focus on parallel for loops in OpenMP,
+though you may find tasks useful in certain places.
+
+For each of these kernels, add the appropriate `#pragma`s to the code to
+parallelize the kernel as much as possible.
+Feel free to make changes to the code (such as reorganizing code, loops, etc.)
+if you find that these changes can allow you to expose additional parallelism.
+For each kernel, document (using comments) your decisions and the changes you
+make to the code; explain why you made the changes you did, and especially when
+you add parallelism explain why it is safe to do so.
+Also, try to explain why some parts of the program can't be parallelized.
+
+Note that not all kernels have the same types of parallelism, and we anticipate
+the difficulty of parallelizing the kernels will vary.
+Additionally, not all kernels may be amenable to parallelization, in these
+cases try to explain why.
+
+Ultimately, while we want to see your ability to parallelize these kernels, we
+are most interested in seeing you explain your thought process and how you
+approached these problems.
+Demonstrating effort and thought is as important as performing the
+parallelization.
+Note that we will not be evaluating you based on the performance of your code,
+we are just interested in you identifying the parallelism in these kernels, so
+do not worry about optimizing the kernels or your parallelism.
+The code you submit should compile and run and produce the same results as the
+sequential versions we provide, and your versions should not introduce any
+memory errors or leaks (note that OpenMP does not always properly exit its
+threads which can lead to valgrind reporting memory leaks, you do not have to
+worry about these leaks).
+OpenMP is available in modern versions of both GCC and Clang for you to test
+your code, please document at the top of your submission the compiler you used
+and its version.
+
+Again, make sure to document your thought process; explain how and why a kernel
+can be parallelized, even if you're not able to implement the parallelism we
+want to see evidence that you've thought about it.
+
+**This assignment is due by Midnight (in Urbana, IL) on Friday, March 11,
+2022.**
+
+Directions for submitting this assignment, and for who to contact if you have
+difficulties, were provided in the email.
+
+If you complete the above portions of this assignment and have more time and
+want to do more, consider attempting the following.
+
+**Challenge 1:** For the 5th kernel (product of a list of matrices) we note in
+the source code that you may assume all matrices are square n x n matrices. If
+your parallelization used this assumption, now implement a version which does
+not require this assumption.
+
+**Challenge 2:** There is some opportunity for parallelism outside of the
+kernels (specifically either within `main` or our definition of the `Matrix`
+class). Parallelize the rest of the program as much as possible; if you find
+it useful you may perform arbitrary restructructing of the code we provide, as
+long as the Kernels remain their own functions.
